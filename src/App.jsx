eacc20cd74dc0822659a2b7ff09738c1dba91e50
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-05-27 01:42:07 -0600

    styles applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Heater1 from './audio/Heater-1.mp3'
 import Heater2 from './audio/Heater-2.mp3'
@@ -12,6 +12,8 @@ import ClosedHH from './audio/Closed-HH.mp3'
 import DrumPad from './components/DrumPad'
 
 function App () {
+  const [currentSound, setCurrentSound] = useState('')
+
   const padBankInfo = [
     { btnId: 'Heater-1', keyboardKey: 'Q', soundSource: Heater1 },
     { btnId: 'Heater-2', keyboardKey: 'W', soundSource: Heater2 },
@@ -24,9 +26,10 @@ function App () {
     { btnId: 'Closed-HH', keyboardKey: 'C', soundSource: ClosedHH }
   ]
 
-  const playSound = (soundId) => {
-    const audio = document.getElementById(soundId)
+  const playSound = (soundKey, btnId) => {
+    const audio = document.getElementById(soundKey)
     if (audio !== null) {
+      setCurrentSound(btnId)
       audio.play()
     }
   }
@@ -37,9 +40,10 @@ function App () {
 
   return (
     <>
-      <div id='drum-machine' className='d-flex justify-content-center align-items-center h-100'>
-        <div className='d-flex'>
-          <div id='pad-bank'>
+      <div id='drum-machine' className='d-flex flex-column justify-content-center align-items-center h-100 w-100'>
+        <h1 className='general-title'>Drum Machine</h1>
+        <div id='drum-machine-container' className='d-flex rounded shadow'>
+          <div id='pad-bank' className='d-flex flex-wrap justify-content-center align-items-center gap-2 py-4'>
             {padBankInfo.map(drumPad => (
               <DrumPad
                 key={drumPad.keyboardKey}
@@ -50,8 +54,12 @@ function App () {
               />
             ))}
           </div>
-          <div id='controls'>
-            <p id='display'> </p>
+          <div id='controls' className='d-flex flex-column align-items-center justify-content-center py-4'>
+            <div className='form-check form-switch d-flex flex-column align-items-center p-0 mb-3'>
+              <label className='form-check-label p-0 m-0' htmlFor='flexSwitchCheckChecked'>Power</label>
+              <input className='form-check-input p-0 m-0' type='checkbox' role='switch' id='flexSwitchCheckChecked' checked />
+            </div>
+            <p id='display' className='d-flex align-items-center justify-content-center m-0 rounded'>{currentSound}</p>
           </div>
         </div>
       </div>
--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const DrumPad = ({ btnId, keyboardKey, soundSource, handlePlaySound }) => {
   return (
-    <button className='drum-pad' id={btnId} onClick={() => handlePlaySound(keyboardKey)}>
+    <button className='drum-pad btn btn-custom shadow' id={btnId} onClick={() => handlePlaySound(keyboardKey, btnId)}>
       {keyboardKey}
       <audio src={soundSource} className='clip' id={keyboardKey} />
     </button>
